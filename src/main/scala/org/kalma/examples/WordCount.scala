51c7cec7cd9fbec77f5215976275e6d5eceb9e1b
MarioLopezz <mlopezzbueno@gmail.com>
2022-06-10 08:31:32 +0200

    fix

--- src/main/scala/org/kalma/examples/WordCount.scala
+++ src/main/scala/org/kalma/examples/WordCount.scala
@@ -1,3 +1,5 @@
+package org.kalma.examples
+
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.rdd.RDD
 
--- src/main/scala/org/kalma/examples/readWriteHdfs.scala
+++ src/main/scala/org/kalma/examples/readWriteHdfs.scala
@@ -1,6 +1,6 @@
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions._
 
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{col, lit}
 
 object readWriteHdfs {
   def main(args: Array[String]) {
@@ -15,7 +15,7 @@ object readWriteHdfs {
     println()
     println()
     println()
-    val df = Seq((1, 2, "APP Name :" + spark.sparkContext.appName), (3, 4,"Deploy Mode :" + spark.sparkContext.deployMode), (5,6,"Master :" + spark.sparkContext.master)).toDF("Col_0", "Col_1", "col3")
+    val df = Seq((1, 2, "APP Name :" + spark.sparkContext.appName), (3, 4, "Deploy Mode :" + spark.sparkContext.deployMode), (5, 6, "Master :" + spark.sparkContext.master)).toDF("Col_0", "Col_1", "col3")
     df.show(false)
 
     df.write.mode("overwrite").format("csv").save("hdfs://hdfs-namenode-sts-0.hdfs-namenode-service.default.svc.cluster.local/datosSc.csv")
